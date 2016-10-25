433d4b47164d909d2a7e0224dc4e6b865ae12f4a
IreneRodriguezMar <irene.rodriguezmar@gmail.com>
2016-10-25 15:24:03 +0000

    VERSIÓN 0.0.2

--- TRELLOV1/js/main.js
+++ TRELLOV1/js/main.js
@@ -1,31 +1,46 @@
 function añadir() {
+	
+	//con esa sintaxis se podra averiguar quien es el padre de ese elemento
+	//document.getElementById("btnañadirlista").parentElement);
+	 
+	var div = document.getElementById("btnañadirlista").parentElement;
     var añadeInputs = document.createElement("input");
-		añadeInputs.setAttribute("class","text-center");
-    var textInput = document.createTextNode("Añadir una lista...");
-		
-    añadeInputs.appendChild(textInput);
-    document.body.appendChild(añadeInputs);
+		añadeInputs.setAttribute("placeholder","Añadir una lista...");
+	
+	//que mi input (añadeInputs) lo guarde como hijo de div, en este caso mi boton
+		div.appendChild(añadeInputs);
 	
 	var guardar = document.createElement("button");
 		guardar.setAttribute("class","btn-success");
     var textbtn = document.createTextNode("Guardar");
-		
-    guardar.appendChild(textbtn);
-    document.body.appendChild(guardar);
 	
-	var lista = document.getElementById("lista");
-	var texto = document.getElementById(textInput);
+	//que el nodo de texto lo guarde en mi boton gurdar y este lo guarde como hijo de div
+		guardar.appendChild(textbtn);
+    	div.appendChild(guardar);
 	
-	var guardarLista = function (){
-		var listaAñadida = document.createElement("li");
-			listaAñadida.setAttribute("class", "list-group-item");
-		var textoContenido = document.createTextNode(listaAñadida);
+	//que el boton que cree se active con onclick y con la funcion le digo que me cree un h3 con un nodo de texto	
+		guardar.onclick = function (){
+	var añadirtitulo = document.createElement("h3");
+	var textoContenido = document.createTextNode(añadeInputs.value);
+			
+	//que el elemento nodo de texto sea guardado como hijo de (añadirtitulo) y este lo guarde como hijo de div	
+		añadirtitulo.appendChild(textoContenido); 
+		div.appendChild(añadirtitulo);
+			
+	//que mi input creado lo deje en blanco cada se le da click	
+		añadeInputs.value ="";
+			
+	//		
+	var añadirTarjeta = document.createElement("a");
+		añadirTarjeta.setAttribute("href", "#");
+	var textañadeTarjeta = document.createTextNode("añadir una tarjeta");		
+			añadirTarjeta.appendChild(textañadeTarjeta);
+			div.appendChild(añadirTarjeta);
 		
-		listaAñadida.appendChild(textoContenido); 
-		lista.appendChild(listaAñadida);
-		
-		textInput.value ="";
 	}
+	//que mi boton de añadir lista sea reemplazado por mi input con boton	
+ // div.replaceChild() 
+	
 
 }
 
